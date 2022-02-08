0f4d209078996b89a53d67a50601529e678cab74
Geoff Higginbottom <github@geoffh.co.uk>
2022-02-08 17:11:44 +0000

    Cleaned up LB Configuration

--- modules/instances/loadbalancer-listeners.tf
+++ modules/instances/loadbalancer-listeners.tf
@@ -1,19 +1,19 @@
-resource "aws_lb_listener" "gateway-lb-listener-9411" {
+resource "aws_lb_listener" "gateway-lb-listener-4318" {
   default_action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-9411.arn
+    target_group_arn = aws_lb_target_group.gateway-lb-tg-4318.arn
     type = "forward"
   }
   load_balancer_arn = aws_lb.gateway-lb.arn
-  port = 9411
+  port = 4318
   protocol = "HTTP"
 }
 
-resource "aws_lb_listener_rule" "rule-9411" {
-  listener_arn = aws_lb_listener.gateway-lb-listener-9411.id
+resource "aws_lb_listener_rule" "rule-4318" {
+  listener_arn = aws_lb_listener.gateway-lb-listener-4318.id
   priority = 100
 
   action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-9411.arn
+    target_group_arn = aws_lb_target_group.gateway-lb-tg-4318.arn
     type = "forward"
   }
 
@@ -23,7 +23,6 @@ resource "aws_lb_listener_rule" "rule-9411" {
     }
   }
 }
-
 
 
 resource "aws_lb_listener" "gateway-lb-listener-9943" {
@@ -52,8 +51,6 @@ resource "aws_lb_listener_rule" "rule-9943" {
   }
 }
 
-
-
 resource "aws_lb_listener" "gateway-lb-listener-6060" {
   default_action {
     target_group_arn = aws_lb_target_group.gateway-lb-tg-6060.arn
@@ -80,87 +77,83 @@ resource "aws_lb_listener_rule" "rule-6060" {
   }
 }
 
+# resource "aws_lb_listener" "gateway-lb-listener-9411" {
+#   default_action {
+#     target_group_arn = aws_lb_target_group.gateway-lb-tg-9411.arn
+#     type = "forward"
+#   }
+#   load_balancer_arn = aws_lb.gateway-lb.arn
+#   port = 9411
+#   protocol = "HTTP"
+# }
 
+# resource "aws_lb_listener_rule" "rule-9411" {
+#   listener_arn = aws_lb_listener.gateway-lb-listener-9411.id
+#   priority = 100
 
-resource "aws_lb_listener" "gateway-lb-listener-7276" {
-  default_action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-7276.arn
-    type = "forward"
-  }
-  load_balancer_arn = aws_lb.gateway-lb.arn
-  port = 7276
-  protocol = "HTTP"
-}
+#   action {
+#     target_group_arn = aws_lb_target_group.gateway-lb-tg-9411.arn
+#     type = "forward"
+#   }
 
-resource "aws_lb_listener_rule" "rule-7276" {
-  listener_arn = aws_lb_listener.gateway-lb-listener-7276.id
-  priority = 100
-
-  action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-7276.arn
-    type = "forward"
-  }
-
-  condition {
-    path_pattern {
-      values = ["/static/*"]
-    }
-  }
-}
-
-
-
-resource "aws_lb_listener" "gateway-lb-listener-4318" {
-  default_action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-4318.arn
-    type = "forward"
-  }
-  load_balancer_arn = aws_lb.gateway-lb.arn
-  port = 4318
-  protocol = "HTTP"
-}
-
-resource "aws_lb_listener_rule" "rule-4318" {
-  listener_arn = aws_lb_listener.gateway-lb-listener-4318.id
-  priority = 100
-
-  action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-4318.arn
-    type = "forward"
-  }
-
-  condition {
-    path_pattern {
-      values = ["/static/*"]
-    }
-  }
-}
+#   condition {
+#     path_pattern {
+#       values = ["/static/*"]
+#     }
+#   }
+# }
 
 
 
 
-resource "aws_lb_listener" "gateway-lb-listener-55681" {
-  default_action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-55681.arn
-    type = "forward"
-  }
-  load_balancer_arn = aws_lb.gateway-lb.arn
-  port = 55681
-  protocol = "HTTP"
-}
+# resource "aws_lb_listener" "gateway-lb-listener-7276" {
+#   default_action {
+#     target_group_arn = aws_lb_target_group.gateway-lb-tg-7276.arn
+#     type = "forward"
+#   }
+#   load_balancer_arn = aws_lb.gateway-lb.arn
+#   port = 7276
+#   protocol = "HTTP"
+# }
 
-resource "aws_lb_listener_rule" "rule-55681" {
-  listener_arn = aws_lb_listener.gateway-lb-listener-55681.id
-  priority = 100
+# resource "aws_lb_listener_rule" "rule-7276" {
+#   listener_arn = aws_lb_listener.gateway-lb-listener-7276.id
+#   priority = 100
 
-  action {
-    target_group_arn = aws_lb_target_group.gateway-lb-tg-55681.arn
-    type = "forward"
-  }
+#   action {
+#     target_group_arn = aws_lb_target_group.gateway-lb-tg-7276.arn
+#     type = "forward"
+#   }
 
-  condition {
-    path_pattern {
-      values = ["/static/*"]
-    }
-  }
-}
+#   condition {
+#     path_pattern {
+#       values = ["/static/*"]
+#     }
+#   }
+# }
+
+# resource "aws_lb_listener" "gateway-lb-listener-55681" {
+#   default_action {
+#     target_group_arn = aws_lb_target_group.gateway-lb-tg-55681.arn
+#     type = "forward"
+#   }
+#   load_balancer_arn = aws_lb.gateway-lb.arn
+#   port = 55681
+#   protocol = "HTTP"
+# }
+
+# resource "aws_lb_listener_rule" "rule-55681" {
+#   listener_arn = aws_lb_listener.gateway-lb-listener-55681.id
+#   priority = 100
+
+#   action {
+#     target_group_arn = aws_lb_target_group.gateway-lb-tg-55681.arn
+#     type = "forward"
+#   }
+
+#   condition {
+#     path_pattern {
+#       values = ["/static/*"]
+#     }
+#   }
+# }
--- modules/instances/loadbalancer-target-groups.tf
+++ modules/instances/loadbalancer-target-groups.tf
@@ -1,11 +1,9 @@
-resource "aws_lb_target_group" "gateway-lb-tg-9411" {
-  name     = "gateway-lb-tg-9411"
-  port     = 9411
+resource "aws_lb_target_group" "gateway-lb-tg-4318" {
+  name     = "gateway-lb-tg-4318"
+  port     = 4318
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   
-#   lifecycle { create_before_destroy=true }
-
   health_check {
     path = "/"
     port = 13133
@@ -17,14 +15,12 @@ resource "aws_lb_target_group" "gateway-lb-tg-9411" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "gateway-lb-tg-9411-attachment" {
+resource "aws_lb_target_group_attachment" "gateway-lb-tg-4318-attachment" {
   count            = var.gateway_count
-  target_group_arn = aws_lb_target_group.gateway-lb-tg-9411.arn
+  target_group_arn = aws_lb_target_group.gateway-lb-tg-4318.arn
   target_id        = aws_instance.gateway[count.index].id
-  port             = 9411
+  port             = 4318
 }
-
-
 
 resource "aws_lb_target_group" "gateway-lb-tg-9943" {
   name     = "gateway-lb-tg-9943"
@@ -52,8 +48,6 @@ resource "aws_lb_target_group_attachment" "gateway-lb-tg-9943-attachment" {
   port             = 9943
 }
 
-
-
 resource "aws_lb_target_group" "gateway-lb-tg-6060" {
   name     = "gateway-lb-tg-6060"
   port     = 6060
@@ -80,86 +74,82 @@ resource "aws_lb_target_group_attachment" "gateway-lb-tg-6060-attachment" {
   port             = 6060
 }
 
-
-
-resource "aws_lb_target_group" "gateway-lb-tg-7276" {
-  name     = "gateway-lb-tg-7276"
-  port     = 7276
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+# resource "aws_lb_target_group" "gateway-lb-tg-9411" {
+#   name     = "gateway-lb-tg-9411"
+#   port     = 9411
+#   protocol = "HTTP"
+#   vpc_id   = var.vpc_id
   
-#   lifecycle { create_before_destroy=true }
+# #   lifecycle { create_before_destroy=true }
 
-  health_check {
-    path = "/"
-    port = 13133
-    healthy_threshold = 5
-    unhealthy_threshold = 2
-    timeout = 2
-    interval = 5
-    matcher = "200"  # has to be HTTP 200 or fails
-  }
-}
+#   health_check {
+#     path = "/"
+#     port = 13133
+#     healthy_threshold = 5
+#     unhealthy_threshold = 2
+#     timeout = 2
+#     interval = 5
+#     matcher = "200"  # has to be HTTP 200 or fails
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "gateway-lb-tg-7276-attachment" {
-  count            = var.gateway_count
-  target_group_arn = aws_lb_target_group.gateway-lb-tg-7276.arn
-  target_id        = aws_instance.gateway[count.index].id
-  port             = 7276
-}
+# resource "aws_lb_target_group_attachment" "gateway-lb-tg-9411-attachment" {
+#   count            = var.gateway_count
+#   target_group_arn = aws_lb_target_group.gateway-lb-tg-9411.arn
+#   target_id        = aws_instance.gateway[count.index].id
+#   port             = 9411
+# }
 
 
 
-resource "aws_lb_target_group" "gateway-lb-tg-4318" {
-  name     = "gateway-lb-tg-4318"
-  port     = 4318
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+# resource "aws_lb_target_group" "gateway-lb-tg-7276" {
+#   name     = "gateway-lb-tg-7276"
+#   port     = 7276
+#   protocol = "HTTP"
+#   vpc_id   = var.vpc_id
   
-#   lifecycle { create_before_destroy=true }
+# #   lifecycle { create_before_destroy=true }
 
-  health_check {
-    path = "/"
-    port = 13133
-    healthy_threshold = 5
-    unhealthy_threshold = 2
-    timeout = 2
-    interval = 5
-    matcher = "200"  # has to be HTTP 200 or fails
-  }
-}
+#   health_check {
+#     path = "/"
+#     port = 13133
+#     healthy_threshold = 5
+#     unhealthy_threshold = 2
+#     timeout = 2
+#     interval = 5
+#     matcher = "200"  # has to be HTTP 200 or fails
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "gateway-lb-tg-4318-attachment" {
-  count            = var.gateway_count
-  target_group_arn = aws_lb_target_group.gateway-lb-tg-4318.arn
-  target_id        = aws_instance.gateway[count.index].id
-  port             = 4318
-}
+# resource "aws_lb_target_group_attachment" "gateway-lb-tg-7276-attachment" {
+#   count            = var.gateway_count
+#   target_group_arn = aws_lb_target_group.gateway-lb-tg-7276.arn
+#   target_id        = aws_instance.gateway[count.index].id
+#   port             = 7276
+# }
 
-
-
-resource "aws_lb_target_group" "gateway-lb-tg-55681" {
-  name     = "gateway-lb-tg-55681"
-  port     = 55681
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+# resource "aws_lb_target_group" "gateway-lb-tg-55681" {
+#   name     = "gateway-lb-tg-55681"
+#   port     = 55681
+#   protocol = "HTTP"
+#   vpc_id   = var.vpc_id
   
-#   lifecycle { create_before_destroy=true }
+# #   lifecycle { create_before_destroy=true }
 
-  health_check {
-    path = "/"
-    port = 13133
-    healthy_threshold = 5
-    unhealthy_threshold = 2
-    timeout = 2
-    interval = 5
-    matcher = "200"  # has to be HTTP 200 or fails
-  }
-}
+#   health_check {
+#     path = "/"
+#     port = 13133
+#     healthy_threshold = 5
+#     unhealthy_threshold = 2
+#     timeout = 2
+#     interval = 5
+#     matcher = "200"  # has to be HTTP 200 or fails
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "gateway-lb-tg-55681-attachment" {
-  count            = var.gateway_count
-  target_group_arn = aws_lb_target_group.gateway-lb-tg-55681.arn
-  target_id        = aws_instance.gateway[count.index].id
-  port             = 55681
-}
+# resource "aws_lb_target_group_attachment" "gateway-lb-tg-55681-attachment" {
+#   count            = var.gateway_count
+#   target_group_arn = aws_lb_target_group.gateway-lb-tg-55681.arn
+#   target_id        = aws_instance.gateway[count.index].id
+#   port             = 55681
+# }
