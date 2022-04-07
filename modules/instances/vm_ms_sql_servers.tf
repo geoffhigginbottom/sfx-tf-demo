a3eaea609423f11d0044b8b390eea50bc2671d1c
Geoff Higginbottom <github@geoffh.co.uk>
2022-04-07 10:14:22 +0100

    Disabled IE Security by default

--- modules/instances/vm_ms_sql_servers.tf
+++ modules/instances/vm_ms_sql_servers.tf
@@ -38,6 +38,9 @@ resource "aws_instance" "ms_sql" {
     Stop-Service splunk-otel-collector
     Start-Service splunk-otel-collector
 
+    Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' -name IsInstalled -Value 0
+    Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' -name IsInstalled -Value 0
+
   </powershell>
   EOF
 
