a895ccbd21cb54cd755dd796e1d497f324fd353f
Geoff Higginbottom <ghigginbottom@splunk.com>
2024-04-18 10:49:49 +0100

    Created role specific versions

---
+++ modules/instances/scripts/install_splunk_universal_forwarder_mysql.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Version 2.0
+
+UNIVERSAL_FORWARDER_FILENAME=$1
+UNIVERSAL_FORWARDER_URL=$2
+PASSWORD=$3
+SPLUNK_IP=$4
+
+wget -O $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL
+sudo dpkg -i $UNIVERSAL_FORWARDER_FILENAME
+sudo /opt/splunkforwarder/bin/splunk cmd splunkd rest --noauth POST /services/authentication/users "name=admin&password=$PASSWORD&roles=admin"
+sudo /opt/splunkforwarder/bin/splunk start --accept-license
+sudo /opt/splunkforwarder/bin/splunk stop
+sudo /opt/splunkforwarder/bin/splunk enable boot-start
+sudo /opt/splunkforwarder/bin/splunk start
+sudo /opt/splunkforwarder/bin/splunk add forward-server $SPLUNK_IP:9997 -auth admin:$PASSWORD    # adds to /opt/splunkforwarder/etc/system/local/outputs.conf
+sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/syslog -auth admin:$PASSWORD           # adds to /opt/splunkforwarder/etc/apps/search/local/inputs.conf
+sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/mysql -auth admin:$PASSWORD          # adds to /opt/splunkforwarder/etc/apps/search/local/inputs.conf
