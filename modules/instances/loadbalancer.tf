a090225b1803ebf68d3d89f66a9a6bae8ee9accb
Geoff Higginbottom <ghigginbottom@splunk.com>
2024-05-22 10:30:51 +0100

    Update loadbalancer.tf

--- modules/instances/loadbalancer.tf
+++ modules/instances/loadbalancer.tf
@@ -1,8 +1,9 @@
 resource "aws_lb" "gateway-lb" {
-  name                = "${var.environment}-gateway-lb"
-  internal            = true
-  load_balancer_type  = "application"
-  security_groups     = [aws_security_group.instances_sg.id]
-  subnets = var.public_subnet_ids
+  name                       = "${var.environment}-gateway-lb"
+  internal                   = true
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.instances_sg.id]
+  subnets                    = var.public_subnet_ids
   enable_deletion_protection = false
+  drop_invalid_header_fields = true
 }
